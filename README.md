ebb57e81cd2ed530fd44a84e1e87bed487003490
Oshchenkov <oshchenkovv@gmail.com>
2019-08-02 13:17:38 +0300

    start template

---
+++ README.md
@@ -0,0 +1,8 @@
+# OptimizedSassTemplate
+Optimized SASS template with gulp. Ready for gitHub /doc demo show.
+
+For installing run in main folder "npm install"
+
+For starting gulp run: "gulp"
+
+For creating /doc folder for showing result in gitHub, run: "gulp demo".
---
+++ app/js/common.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const log = console.log.bind(console);
+
+$('#exampleModal').modal('show');
+
+
+let currentValue = 100;
+let maxRange = 1200;
+let inputBtn = $('.inputButtons__inputBtn');
+let outputText = document.getElementById('inputBlock__inputTextValue');
+let rangeBlockAmount = document.getElementById('rangeBlock__amount');
+let rangeBlockCurrent = document.getElementById('rangeBlock__current');
+let inputTextValue = document.getElementById('inputBlock__inputTextValue');
+
+
+
+$(document).ready(function () {
+    outputText.value = currentValue;
+    changeRangeLines();
+});
+
+
+function changeRange(currentValue){
+    if(currentValue >= maxRange){
+        currentValue = maxRange;
+    } else if (currentValue <= 0){
+        currentValue = 0;
+    }
+    outputText.value = currentValue;
+    changeRangeLines();
+}
+
+for (let i = 0; i < inputBtn.length; i++) {
+    inputBtn[i].addEventListener('click', function () {
+        if ($(this).hasClass("inputButtons__up")){
+            if (currentValue < maxRange){
+                currentValue += 10;
+            }
+            
+        } else if ($(this).hasClass("inputButtons__down")){
+            if (currentValue > 0) {
+                currentValue -= 10;
+            }
+        }
+        changeRange(currentValue);
+    });
+}
+
+function changeRangeLines(){
+    let rangeLinesAmountWidth, rangeLinesCurrentWidth;
+
+    rangeLinesAmountWidth = Math.round(((maxRange - currentValue) / maxRange * 100) * 100) / 100;
+    
+    if (rangeLinesAmountWidth >= 0 && rangeLinesAmountWidth <= 100){
+        log(rangeLinesAmountWidth);
+        rangeBlockAmount.style.width = rangeLinesAmountWidth + '%';
+    }
+
+    rangeLinesCurrentWidth = Math.round(((currentValue) / maxRange * 100) * 100) / 100;
+    if (rangeLinesCurrentWidth >= 0 && rangeLinesCurrentWidth <=100){
+        rangeBlockCurrent.style.width = rangeLinesCurrentWidth + '%';
+    }
+}
+
+inputTextValue.addEventListener("focusout", changeTextInput);
+function changeTextInput(){
+    currentValue = inputTextValue.value;
+    log('changeTextInput' + currentValue);
+    changeRange(currentValue);
+}
